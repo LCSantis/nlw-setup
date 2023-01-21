9cff16d03a5a5f216de330f87290b235bbb4383f
LCSantis <lucas.santis@outlook.com.br>
2023-01-21 20:03:36 -0300

    events

--- script.js
+++ script.js
@@ -3,13 +3,35 @@
 
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  run: ["01-01", "01-02", "01-07"],
-  takePills: ["01-03"],
-  journal: ['01-04', '01-07']
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+  
+  if (dayExists) {
+    alert("Dia já incluso!!!")
+    return
+  }
+
+  alert('Adicionado com sucesso!')
+  nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)
+function save() {
+  localStorage.setItem('Santis@db', JSON.stringify(nlwSetup.data))
+}
 
+// const data = {
+//   run: ["01-01", "01-02", "01-07"],
+//   takePills: ["01-03"],
+//   journal: ['01-04', '01-07']
+// }
+
+const data = JSON.parse(localStorage.getItem("Santis@db")) || {}
+nlwSetup.setData(data)
 nlwSetup.load()
